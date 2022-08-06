89ffc67274fbf56178a6a4941829d65a5a63e944
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-08-06 20:57:13 -0300

    Criado pasta styles e o arquivo styles.module com a extensao do sass e no arquivo index para teste adicionei um estilo ao titulo.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import S from '../styles/styles.module.scss'
+
 export default function Home() {
   return (
     <div>
-      <h1>Bem vindo ao projeto Board!</h1>
+      <h1 className={S.title}>Bem vindo ao projeto <span>Board</span>!</h1>
     </div>
   )
 }
